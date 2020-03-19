665fa4c6ef1080e3be2b7dc1427caf36ef20d1f0
Neri-Laredo <nerila1310@gmail.com>
2020-03-18 19:44:43 -0600

    Eliminamos citas del state

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ function App() {
 
 	//funcion que tome las citas actuales y agregue las nuevas
 	const crearCita = cita => {
-		guardarCitas([
-			...citas,
-			cita
-		]);
-	} 
+		guardarCitas([...citas, cita ]);
+	}
+	
+	//Funcion que elimina una cita por su id
+	const eliminarCita = id =>{
+		const nuevasCitas = citas.filter(cita => cita.id !== id );
+		guardarCitas(nuevasCitas);
+	}
+	
 
   	return (
 		<Fragment>
@@ -32,6 +36,7 @@ function App() {
 							<Cita 
 								key={cita.id}
 								cita={cita}
+								eliminarCita={eliminarCita}
 							/>
 						))}
 					</div>
