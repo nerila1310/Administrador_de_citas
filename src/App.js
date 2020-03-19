e46f2db83699e36ec9231cf4482c0a79cf05f0bf
Neri-Laredo <nerila1310@gmail.com>
2020-03-18 19:52:34 -0600

    Creamos titulo dinamico al crear citas

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ function App() {
 		const nuevasCitas = citas.filter(cita => cita.id !== id );
 		guardarCitas(nuevasCitas);
 	}
+
+	//Mensaje condicional
+	const titulo = citas.length === 0 ?'No hay citas' :'Administra tus Citas';
 	
 
   	return (
@@ -31,7 +34,7 @@ function App() {
 						/>
 					</div>
 					<div className="one-half column">
-						<h2>Administra tus citas</h2>
+	  					<h2>{titulo}</h2>
 						{citas.map(cita => (
 							<Cita 
 								key={cita.id}
